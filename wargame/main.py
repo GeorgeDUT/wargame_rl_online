4683543d6fd48df883a0b3bf14f7d2ef3220f59b
GeorgeDUT <zmybsm@qq.com>
2018-06-04 23:36:22 +0800

    add rl

--- wargame/main.py
+++ wargame/main.py
@@ -6,17 +6,23 @@ RED_ARMY = 10
 GRAY_ARMY = 10
 
 class RL(object):
-    def __init__(self,action_space, learning_rate=0.01,reward_decay=0.9,e_greedy=0.9):
+    def __init__(self, action_space, learning_rate=0.01,reward_decay=0.9,e_greedy=0.9):
         self.actions = action_space
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
-        self.q_table = pd.DataFrame(columns=self.actions, type=np.float64)
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float64)
         print(self.q_table)
-        print('a')
+
+class Sarsa(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9, e_greedy=0.9):
+        super(Sarsa, self).__init__(actions, learning_rate, reward_decay, e_greedy)
+    def learn(self, s, a, r, s_, a_):
+        pass
+
 
 def update():
-    for turn in range(1):
+    for turn in range(500):
         env.reload()
         for agentid in range(RED_ARMY):
             num = random.randint(0,3)
@@ -26,7 +32,7 @@ def update():
 
 if __name__ == "__main__":
     env = Warmap()
-    #env.step('d', 9)
     update()
-    test = RL(action_space=['u','d','l','r'])
+    test = Sarsa(actions=['u','d','l','r'])
+
     env.mainloop()
